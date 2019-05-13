92a43679ead7fdc13d0126e0d2e73b1a37067ea8
Samuel Mutemi <mtsammy40@hotmail.com>
2019-05-13 15:19:05 +0300

    Fixed contacts and disabled navbar

--- js/main.js
+++ js/main.js
@@ -354,7 +354,7 @@ $(document).ready(function () {
         function init() {
             var mapOptions = {
                 zoom: 11,
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                center: new google.maps.LatLng(-1.3269713,36.8371636), // New York
                 styles: [{
                     "featureType": "water",
                     "elementType": "geometry",
